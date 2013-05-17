c2a2f47073128e8fe27b20c482b10cda9c6004e5
Jim Frode Hoff <jimfrodehoff@gmail.com>
2013-05-17 02:43:28 +0200

    Finish getDay() and update README

--- CoffeeDB.py
+++ CoffeeDB.py
@@ -13,7 +13,7 @@
 
 import sys
 import sqlite3
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 
 def Pot(numberToday, madeAt):
     return {"numberToday": numberToday, "madeAt": madeAt}
@@ -38,6 +38,8 @@ class CoffeeDB:
     def disconnect(self):
         self.con.close();
 
+    #### Function: putPot
+    ## Saves a new pot into the database
     def putPot(self, pot):
         # Pot: tuble containing numberToday and madeAt. ex: (3, datetime.now() )
         try:
@@ -49,7 +51,9 @@ class CoffeeDB:
             print "Error %s:" % e.args[0]
             return False
 
-
+    #### Function: getNewestPot
+    ## Delivers the newst pot, to make it easy for existing programs to use
+    ## the service
     def getNewestPot(self):
         try:
             max_id = self.cur.execute("SELECT max(id) FROM Pots")
@@ -61,10 +65,35 @@ class CoffeeDB:
             return None
         return True
 
-    def getDay(self, day, month=str(datetime.month), year=str(datetime.year) ):
-        print day,'-',month, '-', year
+    #### Function: getDay
+    ## Delivers all pots from a given day in a given month in a given year
+    ## Defaults to the current day, month and year
+    def getDay(self, day="", month="", year=""):
+        if year == "": year = str(datetime.now().year)
+        if month == "": month = str(datetime.now().month)
+        if day == "": day = str(datetime.now().day)
 
-    def getMonth(self, month, year=""):
+        ts = datetime.strptime(day+month+year, "%d%m%Y").date()
+
+        try:
+            res = self.cur.execute("SELECT numberToday, madeAt FROM Pots "+\
+    "WHERE pots.madeAt BETWEEN '"+str(ts)+" 00:00:00' AND '"+str(ts)+" 23:59:59'")
+
+    #> '"+year+"-"+month+"-"+day+" 00:00:00' ORDER BY madeAt")
+            result = []
+            for row in res.fetchall():
+                result.append(Pot(row[0], row[1]))
+            return tuple(result)
+
+        except sqlite3.Error, e:
+            print "Error %s:" % e.args[0]
+            return None
+
+
+    #### Function: getMonth
+    ## Delivers all pots from a given month in a given year
+    ## Defaults to the current month and year
+    def getMonth(self, month="", year=""):
         if year == "": year = str(datetime.now().year)
         if month == "": month = str(datetime.now().month)
         try:
@@ -72,7 +101,7 @@ class CoffeeDB:
     "WHERE DATETIME(pots.madeAt) < '"+year+"-"+month+"-31 23:59:59' AND "+\
     "DATETIME(pots.madeAt) > '"+year+"-"+month+"-01 00:00:00' ORDER BY madeAt")
             result = []
-            for row in data:
+            for row in data.fetchall():
                 result.append(Pot(row[0], row[1]))
             return tuple(result)
 
@@ -80,14 +109,17 @@ class CoffeeDB:
             print "Error %s:" % e.args[0]
             return None
 
-    def getYear(self, year):
+    #### Function: getYear
+    ## Delivers all pots from a given month in a given year
+    ## Defaults to the current month and year
+    def getYear(self, year=""):
         if year == "": year = str(datetime.now().year)
         try:
             data = self.cur.execute("SELECT numberToday, madeAt FROM Pots "+\
     "WHERE DATETIME(pots.madeAt) < '"+year+"-12-31 23:59:59' AND "+\
     "DATETIME(pots.madeAt) > '"+year+"-01-01 00:00:00' ORDER BY madeAt")
             result = []
-            for row in data:
+            for row in data.fetchall():
                 result.append(Pot(row[0], row[1]))
             return tuple(result)
 
@@ -95,11 +127,13 @@ class CoffeeDB:
             print "Error %s:" % e.args[0]
             return None
 
+    #### Function: getAll
+    ## Returns every pot ever tracked by CoffeBot. Use with care
     def getAll(self):
         try:
             data = self.cur.execute("SELECT numberToday, madeAt FROM Pots ORDER BY madeAt")
             result = []
-            for row in data:
+            for row in data.fetchall():
                 result.append(Pot(row[0], row[1]))
             return tuple(result)
 
@@ -113,12 +147,16 @@ class CoffeeDB:
             "numberToday INT, madeAt TIMESTAMP)")
 
 
-# Main class, will only be called if this file is run directly
-def main():
-    DEBUG = True
+#### Main function
+## This will only be called if this file is run indepentantly
+def main(INSTALL=False, DEBUG=False):
+
+    PATH = 'notify.db'
+    if DEBUG: PATH = 'coffee_test.db'
+
     testPots = (
         Pot(1, datetime.now().replace(year=2012)),
-        Pot(1, datetime.now().replace(month=1, hour=12)),
+        Pot(1, datetime.now().replace(month=1, day=1, hour=12)),
         Pot(2, datetime.now().replace(month=1, hour=15)),
         Pot(1, datetime.now().replace(day=15)),
         Pot(1, datetime.now().replace(hour=2)),
@@ -126,38 +164,25 @@ def main():
         Pot(3, datetime.now().replace(hour=16))
     )
 
-    db = CoffeeDB('coffee_test.db')
+    db = CoffeeDB(PATH)
 
     db.connect()
-    if DEBUG:
+    if INSTALL:
         db.install_db()
         for pot in testPots:
             print db.putPot(pot)
 
-
-    # All
-    data = db.getAll()
-    print 'All - ', data
-
-    # Newest
-    data = db.getNewestPot()
-    print 'Newest -', data
-
-    # Year
-    data = db.getYear("2012")
-    print 'Year -', data
-
-    # Month
-    data = db.getMonth("01")
-    print 'Month -', data
-
-    # Day
-    data = db.getDay("17")
-    print 'Day -', data
-
+    # All functions printed
+    print 'All - ', db.getAll()
+    print 'Newest -', db.getNewestPot()
+    print 'Year -', db.getYear("2012")
+    print 'Month -', db.getMonth("01")
+    print 'Day -', db.getDay("15")
 
     db.disconnect()
 
 if __name__ == '__main__':
-    main()
+    DEBUG = len(sys.argv) and "debug" in sys.argv
+    INSTALL = len(sys.argv) and "install" in sys.argv
+    main(INSTALL, DEBUG)
 
---
+++ README.md
@@ -0,0 +1,19 @@
+CoffeeBot
+=========
+
+Install
+-------
+
+In order to set up the server, simply run 'python CoffeeDB.py install', and 
+it will generate a sqlite3-file in the crrent working directory, and populate 
+it with data.
+
+If you want to debug and contribute, without ruining your running database, use
+*python CoffeeDB.py install debug* to start a test-database, and use 
+*python CoffeeDB.py debug* to use the testing database without populationg 
+data.
+
+
+
+
+
