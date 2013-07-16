71f3b800447a27cc01532c7211109cb751471e08
Jim Frode Hoff <jimfrodehoff@gmail.com>
2013-07-16 17:47:59 +0200

    Initial node commit

---
+++ routes/coffee.js
@@ -0,0 +1,19 @@
+
+/*
+ * GET users listing.
+ */
+
+exports.index = function(req, res){
+  res.send("respond with a resource");
+};
+ 
+exports.online = {
+	"index": function(req, res){
+		res.send("ONLINE: Index page");
+	},
+	"all": function(req, res){
+	  res.send("ONLINE: All pots page");
+	},
+};
+ 
+ 
