e954588b3c42793983ef1dbee386348dc979d5a5
Jim Frode Hoff <jimfrodehoff@gmail.com>
2013-07-20 01:16:04 +0200

    Add hooks for new methods

--- routes/coffee.js
+++ routes/coffee.js
@@ -14,6 +14,19 @@ db.open(function(err, db) {
     }
 });
 
+var getOldFormatFromDate = function(date) {
+	var monthNames = [ "January", "February", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November", "December" ];
+		
+	var txt = date.getDate() + ". "
+		+ monthNames[date.getMonth()] + " "
+		+ date.getFullYear() + " "
+		+ ((date.getHours() < 10) ? "0" : "") + date.getHours() + ":" 
+		+ ((date.getMinutes() < 10) ? "0" : "") + date.getMinutes() + ":" 
+		+ ((date.getSeconds() < 10) ? "0" : "") + date.getSeconds();
+	return txt;
+}
+
 
 /*
  * GET users listing.
@@ -26,11 +39,15 @@ exports.index = function(req, res){
 // Tenk at en linjeforening == en coll(ection)
 exports.coll = {
 	"index": function(req, res){
-		res.header("Content-Type", "text/html; charset=utf-8");
-		res.render("collection_index", {title: req.params.collection});
+		res.setHeader("Content-type", "text/html; charset=utf-8");
+		
+		console.log( res.render("collection_index", {title: req.params.collection}) );
+		res.render("collection_index", {title: req.params.collection}, function(value) { 
+			console.log(res);
+		});
 	},
 	"all": function(req, res){
-		res.header("Content-Type", "application/json; charset=utf-8");
+		res.setHeader("Content-Type", "application/json; charset=utf-8");
 		
 		console.log("Getting all pots from "+req.params.collection.toUpperCase());
 		db.collection(req.params.collection.toLowerCase(), function(err, collection) {
@@ -40,11 +57,53 @@ exports.coll = {
 					+ '"pots" : ' + JSON.stringify(items) 
 					+ '}';
 
-				res.setHeader("Content-Type", "application/json");
 				res.end(output);
 			});
 		});
 	},
+	"latest":function(req, res) {
+		res.setHeader("Content-Type", "application/json; charset=utf-8");
+		
+		console.log("Getting freshest pot from "+req.params.collection.toUpperCase());
+		db.collection(req.params.collection.toLowerCase(), function(err, collection) {
+			/* Ugly hack while waiting for native MongoDB.collection.MAX(field) */
+			collection.find().sort({"date":-1}).limit(1).toArray(function(err, item) {
+				output = '{'
+					+ '"count" : ' + item.length + ','
+					+ '"pots" : ' + JSON.stringify(item) 
+					+ '}';
+
+				res.end(output);
+			});
+		});
+	},
+	"latest_txt":function(req, res) {
+		res.setHeader("Content-Type", "text/plain; charset=utf-8");
+		
+		console.log("Getting freshest pot from "+req.params.collection.toUpperCase());
+		db.collection(req.params.collection.toLowerCase(), function(err, collection) {
+		
+			/* Ugly hack while waiting for native MongoDB.collection.MAX(field) */
+			collection.find().sort({"date":-1}).limit(1).toArray(function(err, items) {
+				try {
+					var item = items[0];
+					console.log(item);	
+					var output = item.numberThisDay + "\n" 
+							+ getOldFormatFromDate(item.date);
+	
+					res.end(output);
+				} catch(err) {
+					throw err;
+				}
+			});
+		});
+	},
+	"since":function(req, res) {
+		// TODO: Implementere
+		res.status(501);
+		res.end();
+	},
+	
 };
  
  
